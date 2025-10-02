55eea7488ecfaa52f9c0cc5472a9a4da65e41478
DeathStalker471 <thomasnealson85@gmail.com>
2025-10-02 13:20:57 -0700

    revert changes, no reason to use this branch

--- page/vttes.meta.js
+++ page/vttes.meta.js
@@ -26,6 +26,6 @@
 // @webRequest [{"selector":{"include":"*://app.roll20.net/v2/js/patience.js","exclude":"*://app.roll20.net/v2/js/patience.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/editor/startjs/?timestamp*","exclude":"*://app.roll20.net/editor/startjs/?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/d20/loading.js?v=11","exclude":"*://app.roll20.net/js/d20/loading.js?n=11&v=11"},"action":"cancel"}]
-// @webRequest [{"selector":{"include":"*://cdn.roll20.net/vtt/legacy/production/20250930/vtt.bundle.*.js","exclude":"*://cdn.roll20.net/vtt/legacy/production/20250930/vtt.bundle.*.js?n*"},"action":"cancel"}]
+// @webRequest [{"selector":{"include":"*://cdn.roll20.net/vtt/legacy/production/latest/vtt.bundle.*.js","exclude":"*://cdn.roll20.net/vtt/legacy/production/latest/vtt.bundle.*.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/tutorial_tips.js","exclude":"*://app.roll20.net/js/tutorial_tips.js?n*"},"action":"cancel"}]
 // ==/UserScript==
